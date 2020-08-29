e10c3d44394da0b00c3d8ac71a7697105813a3ee
SilenMe <vivek2vks@gmailcom>
2020-08-29 19:22:41 +0530

    Updates

---
+++ precache-manifest.a9ea48bd647cca65100df986bc1ac288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6639e1ec6ac0a1ac1734ca086f126ce1",
+    "url": "/AI-News-application-react/index.html"
+  },
+  {
+    "revision": "a94785e290e5481a765f",
+    "url": "/AI-News-application-react/static/css/main.5dccd743.chunk.css"
+  },
+  {
+    "revision": "9f05d1fd231da30554f0",
+    "url": "/AI-News-application-react/static/js/2.e679940b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/AI-News-application-react/static/js/2.e679940b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94785e290e5481a765f",
+    "url": "/AI-News-application-react/static/js/main.600158c2.chunk.js"
+  },
+  {
+    "revision": "2dac1e0720d0d883c8f6",
+    "url": "/AI-News-application-react/static/js/runtime-main.24e392a1.js"
+  }
+]);
